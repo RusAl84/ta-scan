dbc1a751935d0907082fbdd85160382d36ed17f4
Aleksey <rusal@bk.ru>
2023-01-14 21:24:37 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+﻿from flask import Flask, jsonify, render_template, request, send_from_directory
+from flask_cors import CORS, cross_origin
+import os
+import time
+import config as cfg
+
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+@app.route('/')
+def dafault_route():
+    return 'API'
+
+
+@app.route('/uploadsa', methods=['POST'])
+def uploadsa():
+    if request.method == 'POST':
+        f = request.files['File']
+        # filename = secure_filename(f.filename)
+        milliseconds = int(time.time() * 1000)
+        filename = f"./uploads/{milliseconds}.pcap"
+        f.save(filename)
+        # text = conv.convertJsonMessages2text(filename)
+        # str1 = text
+        from scan_detector import all_check
+        str1 = all_check(filename)
+        print(str1)
+        str1 += "<br> <a href=""javascript:history.back()"">Назад</a>"
+        return str1
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5001")
+# app.run(host="0.0.0.0")
